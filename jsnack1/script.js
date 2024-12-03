3a7676cc25fe1977a555d70990bc24a2e30c6db5
RiccardoOrlando <riccardoorlando08@gmail.com>
2024-12-03 11:39:29 +0100

    feat: snack1

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+const num1 = prompt("inserisci il primo numero")
+const num2 = prompt("inserisci il secondo numero")
+if(num1 > num2) {
+    alert("il primo numero è più grande del secondo")
+}
+else if(num2 > num1) {
+    alert("il secondo numero è più grande del primo")
+}
+else{
+    alert("i due numeri sono uguali")
+}
